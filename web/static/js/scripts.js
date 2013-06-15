fe4a73a62690e4a9b48f5aaa8a4010885a260316
Ali Ismayilov <ali@ismailov.info>
2013-06-15 17:03:21 +0500

    ajaxed add todo

--- web/static/js/scripts.js
+++ web/static/js/scripts.js
@@ -1,14 +1,46 @@
 $(function() {
     $('.todos :checkbox').each(function (index) {
-        var todoId = $(this).attr('id').replace('checkbox', '');
+        var todoId = $(this).attr('id').replace('checkbox-', '');
         $(this).click(function () {
             ajaxCompleted(todoId);
         });
+    });
+
+    $('#add-todo').submit(function () {
+        addTodo();
+        return false;
     });
 });
 
 function ajaxCompleted(id) {
     $.post('/api/todos/' + id + '/completed/', function (data) {
         console.log(data);
+    });
+}
+
+var checkbox;
+
+function addTodo() {
+    var todo = {
+        title: $('#todo-title').val()
+    };
+
+    $.post('/api/todos/', todo, function (data) {
+        console.log(data);
+
+        // reset text input
+        $('#todo-title').val('');
+
+        // build html from template and append it to todo list
+        var template = '<li><label class="checkbox"><input type="checkbox" id="checkbox-{{ id }}"> {{ title }}</label></li>';
+        var html = Mustache.to_html(template, data);
+
+        $('.todos').first().append(html);
+
+        // add event handler for checkbox
+        $('#checkbox-' + data.id).click(function () {
+            console.log(data.id);
+            ajaxCompleted(data.id);
+        });
     });
 }
